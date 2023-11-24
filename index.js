0a62717d44f738d04114a671b947f1d8833a2436
sanglangthang1 <sang.danghs0812@hcmut.edu.vn>
2023-11-24 17:18:51 +0700

    first update ver3

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/tkonline/post', async (req,res) => {
       request.input('Makhachhang', sql.CHAR(9), Makhachhang);
       request.input('Manhanvien', sql.CHAR(6), Manhanvien);
 
-      request.execute('ThemTaiKhoanOnline',(err, result) => {
+      request.execute('ThemTaiKhoanOnline',(err, data) => {
         if (err) {
           console.error(err);
           res.status(500).send('Internal Server Error');
@@ -62,7 +62,7 @@ app.delete('/tkonline/delete/:id', async(req,res) => {
     .then(pool => {
       const request = new sql.Request(pool);
       request.input('Mataikhoan',sql.CHAR(6),req.params.id);
-      request.execute('XoaTaiKhoan',(err,result) => {
+      request.execute('XoaTaiKhoan',(err,data) => {
         if (err) {
           console.error(err);
           res.status(500).send('Internal Server Error');
@@ -89,7 +89,7 @@ app.put('/tkonline/edit', async(req,res) => {
       request.input('Makhachhang', sql.CHAR(9), Makhachhang);
       request.input('Manhanvien', sql.CHAR(6), Manhanvien);
 
-      request.execute('SuaThongTinTaiKhoan',(err, result) => {
+      request.execute('SuaThongTinTaiKhoan',(err, data) => {
         if (err) {
           console.error(err);
           res.status(500).send('Internal Server Error');
@@ -101,8 +101,92 @@ app.put('/tkonline/edit', async(req,res) => {
       })
     })
 })
+
+app.get('/sub/view', async (req,res) => {
+  conn
+    .then(pool => {
+      const request = new sql.Request(pool);
+      sqlString = 'SELECT sc.Masuatchieu, sc.Ngaychieu, sc. Giobatdau, sc.Gioketthuc, p.Tenphim, pcp.Tenphong, rp.Tenrap FROM Suat_chieu sc JOIN Phim p ON sc.Maphim = p.Maphim JOIN Phong_chieu_phim pcp ON sc.Maphong = pcp.Maphong JOIN Rap_phim rp ON pcp.Marap = rp.Marap ORDER BY sc.Ngaychieu';
+      request.query(sqlString, (err, data) => {
+        res.send({result: data.recordset});
+      })
+    })
+})
+
+app.get('/sub/search/:id', async (req,res) => {
+  conn
+    .then(pool => {
+      const request = new sql.Request(pool);
+      request.input('RapPhimId',sql.CHAR(4),req.params.id)
+      request.execute('Tim_Suat_Chieu', (err, data) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        }
+        else {
+          console.log(`tim suat chieu cua rap phim id: ${req.params.id}`);
+          res.status(200).send({result: data.recordset});
+        }
+      })
+    })
+})
+
+app.get('/sub/edit', async (req,res) => {
+  conn
+    .then(pool => {
+      const request = new sql.Request(pool);
+      
+    })
+})
+
+app.get('/statistics/view/:year', async(req,res) => {
+  conn
+    .then(pool => {
+      const request = new sql.Request(pool);
+      request.input('Year', sql.CHAR(4), req.params.year);
+      request.execute('GetUserAccountInfoAfter', (err,data) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+        }
+        else {
+          console.log(`running Get user account after...${req.params.year}`);
+          res.status(200).send({result: data.recordset});
+        }
+      })
+    })
+})
+
+app.get('/availableseats/view/:id', async(req,res) => {
+  conn
+    .then(pool => {
+      const request = new sql.Request(pool);
+      sqlString = `DECLARE @Masuatchieu CHAR(6) = '${req.params.id}'
+      SELECT dbo.TinhSoGheConTrong(@Masuatchieu) AS Soghecontrong, sc.Masuatchieu
+      FROM Suat_chieu sc
+      WHERE @Masuatchieu = sc.Masuatchieu`;
+      request.query(sqlString, (err,data) => {
+        res.send({result: data.recordset});
+      })
+    })
+})
+
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/view' + '/index.html');
+});
+
+app.get('/sub', (req, res) => {
+  res.sendFile(__dirname + '/view' + '/sub.html');
+});
+
+app.get('/statistics', (req, res) => {
+  res.sendFile(__dirname + '/view' + '/statistics.html');
+});
+
+app.get('/availableseats', (req, res) => {
+  res.sendFile(__dirname + '/view' + '/seat.html');
 });
 
 const PORT = process.env.PORT || 3000;
